2a1bcea88035839c94e85b379abb74410620d794
Brian Shewan <w0111036@nscc.ca>
2022-02-01 15:24:57 -0400

    completed the assignment2 demo

--- complete-demo/src/App.js
+++ complete-demo/src/App.js
@@ -1,18 +1,44 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import './App.css';
 
-function App() {
+function App(props) {
 
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(new Date());
 
     const buttonClick = () => {
-        setCount(prev => prev + 1);
+        setCount(prev => prev + props.countBy);
     }
+
+    // runs after every render
+    useEffect(() => {
+        const h1 = document.getElementsByTagName('h1');
+        for (const h of h1) {
+            h.style.backgroundColor = 'green'
+        }
+    })
+
+    // runs when count changes
+    useEffect(() => {
+        const h1 = document.getElementsByTagName('h1');
+        for (const h of h1) {
+            h.style.fontSize = `${count}px`
+        }
+    }, [count]);
+
+    // runs just when the component is started
+    useEffect(() => {
+        const t = setInterval(() => {
+            setTime(new Date())
+        }, 1000);
+        return () => clearInterval(t);
+    }, [])
 
   return (
       <div className='App'>
+        <strong>{time.toLocaleTimeString()}</strong>
         <h1 id='count'>{count}</h1>
-        <button onClick={buttonClick}>Add</button>
+        <button onClick={buttonClick} style={{fontSize: props.fontSize}}>Add</button>
       </div>
   );
 }
--- complete-demo/src/index.js
+++ complete-demo/src/index.js
@@ -4,9 +4,10 @@ import App from './App';
 
 ReactDOM.render(
     <>
-        <App />
-        <App />
-        <App />
+        <App countBy={1} fontSize={12} />
+        <App countBy={2} fontSize={24} />
+        <App countBy={10} fontSize={36} />
+        <button>Do Nothing</button>
     </>,
   document.getElementById('root')
 );
